919780e907fa8a412f805be496bcc691ec0ea0f5
Victor Lambert <vrlambert@gmail.com>
2017-09-13 15:59:58 -0400

    feat: completed the naive bayes exercise

--- Problem Sets/Machine Learning/naive_bayes/nb_author_id.py
+++ Problem Sets/Machine Learning/naive_bayes/nb_author_id.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -25,9 +27,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+def classify(features_train, labels_train):
+    ### import the sklearn module for GaussianNB
+    classifier = GaussianNB()
+    t0 = time()
+    classifier.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 3), "s"
+    return classifier
 
-
+clf = classify(features_train, labels_train)
+t1 = time()
+pred = clf.predict(features_test)
+print "pred time:", round(time() - t1, 3), "s"
+print accuracy_score(pred, labels_test)
 #########################################################
-
-
