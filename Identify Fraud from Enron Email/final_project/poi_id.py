525262c64ab47e91884a30e0a5030af437fda03b
Victor Lambert <vrlambert@gmail.com>
2017-10-11 20:29:44 -0400

    feat: change dump code to dump best classifier

--- Identify Fraud from Enron Email/final_project/poi_id.py
+++ Identify Fraud from Enron Email/final_project/poi_id.py
@@ -82,4 +82,4 @@ print 'recall', metrics.recall_score(labels_test, pred)
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf.best_estimator_, my_dataset, features_list)
